a25d69fb7ec1dfdd49d22a22f147b082cebc412d
Anton Minaichev <anton.minaichev@gmail.com>
2025-03-26 21:25:57 +0300

    fixed flags name to required in task

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -12,9 +12,9 @@ var (
 )
 
 func parseFlags() error {
-	flag.StringVar(&flagRunAddr, "addr", "localhost:8080", "{Host:port} for server")
-	flag.IntVar(&flagReportInterval, "report", 10, "report interval")
-	flag.IntVar(&flagPollInterval, "poll", 2, "poll interval")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "{Host:port} for server")
+	flag.IntVar(&flagReportInterval, "r", 10, "report interval")
+	flag.IntVar(&flagPollInterval, "p", 2, "poll interval")
 	flag.Parse()
 	if len(flag.Args()) > 0 {
 		return fmt.Errorf("unknown flags: %v", flag.Args())
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -8,7 +8,7 @@ import (
 var flagRunAddr string
 
 func parseFlags() error {
-	flag.StringVar(&flagRunAddr, "addr", "localhost:8080", "{Host:port} for server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "{Host:port} for server")
 	flag.Parse()
 	if len(flag.Args()) > 0 {
 		return fmt.Errorf("unknown flags: %v", flag.Args())
