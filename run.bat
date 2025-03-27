2dd999794343ca46f13bcaa9ed795f7c72dfd282
Anton Minaichev <anton.minaichev@gmail.com>
2025-03-27 17:53:59 +0300

    fix /health check

--- run.bat
+++ run.bat
@@ -1,15 +1,23 @@
 @echo off
 echo Starting server and agent...
 
+:: Собираем сервер
+echo Building server...
+go build -o .\cmd\server\server.exe .\cmd\server\
+
+:: Собираем агента
+echo Building agent...
+go build -o .\cmd\agent\agent.exe .\cmd\agent\
+
 :: Запускаем сервер в отдельном окне
-start "Server" cmd /k "go run cmd/server/main.go"
+start "Server" cmd /k ".\cmd\server\server.exe"
 set SERVER_PID=%ERRORLEVEL%
 
 :: Ждем 2 секунды, чтобы сервер успел запуститься
 timeout /t 2
 
 :: Запускаем агента в отдельном окне
-start "Agent" cmd /k "go run cmd/agent/main.go"
+start "Agent" cmd /k ".\cmd\agent\agent.exe -a=localhost:8080 -r=10 -p=2"
 set AGENT_PID=%ERRORLEVEL%
 
 echo Server and Agent are running...
